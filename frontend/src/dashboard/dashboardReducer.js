5b53566746aaa81de4fc98995e8cea2ccc49e720
victormendeslc <victormendeslyma@gmail.com>
2017-11-28 11:36:04 -0200

    Criado Tabs (Abas)

--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -1,4 +1,5 @@
 const INITIAL_STATE = { summary: { credit: 0, debt: 0 } }
+
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'BILLING_SUMMARY_FETCHED':
