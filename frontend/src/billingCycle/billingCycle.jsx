fbcf2fb6d4810ee3615bdc4af22fa6a0c2708bd6
victormendeslc <victormendeslyma@gmail.com>
2017-11-28 17:08:16 -0200

    Mensagens de sucesso e erro.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -9,15 +9,17 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import Tabcontent from '../common/tab/tabContent'
-import {selectTab, showTabs} from '../common/tab/tabActions'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActions'
+
 import List from './billingCycleList';
 import Form from './billingCycleForm'
 
 class BillingCycle extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.selectTab('tabList');
-        this.props.showTabs('tabList','tabCreate');
+        this.props.showTabs('tabList', 'tabCreate');
     }
 
     render() {
@@ -49,10 +51,10 @@ class BillingCycle extends Component {
                         </TabsHeader>
                         <TabsContent>
                             <Tabcontent id="tabList"><h1>Lista</h1>
-                                <List/>
+                                <List />
                             </Tabcontent>
                             <Tabcontent id="tabCreate">
-                                <Form/>
+                                <Form onSubmit={this.props.create} />
                             </Tabcontent>
                             <Tabcontent id="tabUpdate"><h1>Alterar</h1></Tabcontent>
                             <Tabcontent id="tabDelete"><h1>Excluir</h1></Tabcontent>
@@ -65,5 +67,7 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab,showTabs},dispatch)
-export default connect(null,mapDispatchToProps)(BillingCycle)
+const mapDispatchToProps = dispatch => bindActionCreators(
+    { selectTab, showTabs, create }
+    , dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,11 +1,24 @@
-
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3003/api';
+import { toastr } from 'react-redux-toastr'
+const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`);
+    const request = axios.get(`${BASE_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
+    }
+}
+
+export function create(values) {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso!')
+        }, err => {
+            err.response.data.errors.forEach(error => toastr.error('Erro',error));
+        })
+
+    return {
+        type: 'TEMP'
     }
 }
