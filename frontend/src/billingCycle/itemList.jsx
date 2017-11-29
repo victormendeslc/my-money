03a5d21ee4f8eeb68e5c18f7ade2bf52fead21b2
victormendeslc <victormendeslyma@gmail.com>
2017-11-29 16:51:29 -0200

    Adicionando campo status condicional no item list

--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
 
 class ItemList extends Component {
 
@@ -14,9 +15,9 @@ class ItemList extends Component {
         }
     }
 
-    remove(index){
-        if(!this.props.readOnly && this.props.list.length >1){
-            this.props.arrayRemove('billingCycleForm',this.props.field,index)
+    remove(index) {
+        if (!this.props.readOnly && this.props.list.length > 1) {
+            this.props.arrayRemove('billingCycleForm', this.props.field, index)
         }
 
     }
@@ -37,6 +38,14 @@ class ItemList extends Component {
                     readOnly={this.props.readOnly}
                     component={Input}></Field>
                 </td>
+                <If test={this.props.showStatus}>
+                    <td><Field
+                        name={`${this.props.field}[${index}].status`}
+                        placeholder='Informe o status'
+                        readOnly={this.props.readOnly}
+                        component={Input}></Field>
+                    </td>
+                </If>
                 <td className="btn-toolbar col-xs-2">
                     <button type='button' className='btn btn-success'
                         onClick={() => this.add(index + 1)}>
@@ -71,6 +80,9 @@ class ItemList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th>Ações</th>
                             </tr>
                         </thead>
