c824ff69489b38a62ef4b072904ff4360a53407c
victormendeslc <victormendeslyma@gmail.com>
2017-11-29 10:47:05 -0200

    Implementado alterar

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
+
+import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
@@ -12,18 +16,20 @@ class BillingCycleForm extends Component {
                         label='Nome' cols='12 4' placeholder='Informe o nome'
                     />
                     <Field name='month' component={labelAndInput}
-                    type='number' label='Mês' cols='12 4' placeholder='Informe o mês'
+                        type='number' label='Mês' cols='12 4' placeholder='Informe o mês'
                     />
                     <Field name='year' component={labelAndInput}
-                      type='number' label='Ano' cols='12 4' placeholder='Informe o ano'
+                        type='number' label='Ano' cols='12 4' placeholder='Informe o ano'
                     />
                 </div>
                 <div className='box-footer'>
-                    <button type='submit' className='btn btn-primary'
-                    >Submit</button>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
+                    <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
     }
 }
-export default reduxForm({ form: 'billingCycleForm' , destroyOnUnmount: false})(BillingCycleForm)
+BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm);
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
